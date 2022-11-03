da1a4214940083784587b05c4a13646053e3ec42
Danila <danila.ban@mail.ru>
2022-11-03 10:28:56 +0300

    feat: solve second task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,2 +1,28 @@
+const { stdin, stdout } = process;
+const EventEmitter = require('events');
+const emmiter = new EventEmitter();
+const fs = require('fs');
+const path = require('path');
+const output = fs.createWriteStream(path.join(__dirname, 'destination.txt'));
+const readline = require('readline');
+const rl = readline.createInterface ({
+    input: process.stdin,
+    output: process.stdout,
+})
 
+stdin.on('data', () => {})
+
+rl.on('line', (input) => {
+  if (input !== 'exit') {
+      output.write (input + '\n');
+  } else emmiter.emit('exit');
+});
+
+emmiter.on ('exit', () => {
+
+  process.exit()
+
+})
+
+process.on('exit', () => stdout.write ('End of session'));
 
